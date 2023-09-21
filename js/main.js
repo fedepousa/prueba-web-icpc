25baad7d37ca5e3448279be401632d7244d37729
fedepousa <fedepousa@gmail.com>
2023-09-21 10:22:49 -0300

    algunos arreglos

--- js/main.js
+++ js/main.js
@@ -187,11 +187,11 @@ jQuery(document).ready(function($) {
 
 		$('#date-countdown').countdown('2023/10/21', function(event) {
 		  var $this = $(this).html(event.strftime(''
-		    + '<span class="countdown-block"><span class="label">%w</span> weeks </span>'
-		    + '<span class="countdown-block"><span class="label">%d</span> days </span>'
-		    + '<span class="countdown-block"><span class="label">%H</span> hr </span>'
-		    + '<span class="countdown-block"><span class="label">%M</span> min </span>'
-		    + '<span class="countdown-block"><span class="label">%S</span> sec</span>'));
+		    + '<span class="countdown-block"><span class="label">%w</span> Semanas </span>'
+		    + '<span class="countdown-block"><span class="label">%d</span> Dias </span>'
+		    + '<span class="countdown-block"><span class="label">%H</span> Horas </span>'
+		    + '<span class="countdown-block"><span class="label">%M</span> Minutos </span>'
+		    + '<span class="countdown-block"><span class="label">%S</span> Segundos</span>'));
 		});
 				
 	};
